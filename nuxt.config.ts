1c7785d56fb5e37dfb51a530baf8c808a03a1e0c
giopantsulaia <giorgipantsulaia@redberry.ge>
2022-12-12 00:12:21 +0400

    wip: write game logic

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,20 @@
 export default defineNuxtConfig({
-    css: ['~/assets/css/main.css'],
-    postcss: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
     },
+  },
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=500, initial-scale=1',
+      title: 'My App',
+      meta: [
+        // <meta name="description" content="My amazing site">
+        { name: 'description', content: 'My amazing site.' }
+      ],
+    }
+  }
 });
--- pages/play.vue
+++ pages/play.vue
@@ -1,3 +1,29 @@
+<script setup>
+const word = ["c", "h", "a", "r", "m"];
+const answers = useState("answers", () => []);
+const answer = useState("answer", () => ["c", "l", "e", "a", "n"]);
+const correct = useState("correct", () => []);
+
+watch(answer.value, () => {
+    const exists = [];
+    if (answer.value.length >= 5) {
+        for (const [index, letter] in answer.value) {
+            if (answer.value[index] === word[index]) {
+                correct.value.push(index);
+            };
+        }
+    }
+    console.log(correct.value);
+});
+function add(index) {
+    answer.value.push("s");
+}
+</script>
+<style scoped>
+p {
+    text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;
+}
+</style>
 <template>
     <section>
         <nav class="relative top-0 left-0 w-screen border-b-2 border-black h-16 flex justify-center items-center">
@@ -5,9 +31,16 @@
         </nav>
         <main class="mt-16">
             <div class="flex flex-wrap w-[400px]  m-auto gap-4 justify-center">
-                <div class="w-16 h-16 border-2 border-gray-600" v-for="box in 30"></div>
+                <div class="w-16 h-16 flex items-center rounded-lg"
+                    v-for="(box, index) in answers.length === 0 ? 5 : answers[0]">
+                    <p class="w-full h-full text-4xl font-black uppercase text-center pt-2 text-white rounded-lg border-2 border-gray-600"
+                        :class="correct.includes(index.toString()) ? 'bg-green-600' : 'bg-red-500'">{{
+                                answer[index]
+                        }}
+                    </p>
+                </div>
+                <button @click="add(index)">add</button>
             </div>
         </main>
     </section>
 </template>
-<script></script>
