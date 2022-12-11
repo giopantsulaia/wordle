316f9d73768487b4642f58f26144cb7b970c87f9
giopantsulaia <giorgipantsulaia@redberry.ge>
2022-12-11 20:00:12 +0400

    feat: add boxes for words

--- pages/play.vue
+++ pages/play.vue
@@ -1,6 +1,13 @@
 <template>
-    <div class="text-red-500 text-2xl">
-        Hello World!
-    </div>
+    <section>
+        <nav class="relative top-0 left-0 w-screen border-b-2 border-black h-16 flex justify-center items-center">
+            <p class="font-black text-3xl uppercase tracking-widest"> Wordle</p>
+        </nav>
+        <main class="mt-16">
+            <div class="flex flex-wrap w-[400px]  m-auto gap-4 justify-center">
+                <div class="w-16 h-16 border-2 border-gray-600" v-for="box in 30"></div>
+            </div>
+        </main>
+    </section>
 </template>
 <script></script>
